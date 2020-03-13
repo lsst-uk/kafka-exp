bcba2ba3c57c4c2b2aea9ef966c69fbfcc98f575
G Francis <g.francis@epcc.ed.ac.uk>
2020-03-13 12:41:39 +0000

    bugfix

--- swift/write.py
+++ swift/write.py
@@ -4,9 +4,11 @@ from time import time
 import random
 from swiftclient.service import SwiftError, SwiftService, SwiftUploadObject
 from swiftclient.multithreading import OutputManager
+from os import environ 
+from io import BytesIO
 
 # number of objects to write
-n = 10000
+n = 1000
 
 # size of object to write in KB
 size = 15
@@ -25,12 +27,12 @@ _opts = {
     'object_uu_threads': 1,
     'container_threads': 10,
     'segment_threads': 10,
-    "auth_version": os.environ.get('ST_AUTH_VERSION'),  # Should be '3'
-    "os_username": os.environ.get('OS_USERNAME'),
-    "os_password": os.environ.get('OS_PASSWORD'),
-    "os_project_name": os.environ.get('OS_PROJECT_NAME'),
-    "os_project_domain_name": os.environ.get('OS_PROJECT_DOMAIN_NAME'),
-    "os_auth_url": os.environ.get('OS_AUTH_URL')
+    "auth_version": environ.get('ST_AUTH_VERSION'),  # Should be '3'
+    "os_username": environ.get('OS_USERNAME'),
+    "os_password": environ.get('OS_PASSWORD'),
+    "os_project_name": environ.get('OS_PROJECT_NAME'),
+    "os_project_domain_name": environ.get('OS_PROJECT_DOMAIN_NAME'),
+    "os_auth_url": environ.get('OS_AUTH_URL')
 }
 
 if d_type == 0:
@@ -43,14 +45,30 @@ container = c_prefix+str(size)+'_'+str(d_type)
 with SwiftService(options=_opts) as swift:
     start = time()
     for i in range(n):
-        if d_type = 2:
+        if d_type == 2:
             data = bytearray(random.getrandbits(8) for _ in range(size*1024))
         objs = []
         obj = SwiftUploadObject(
             source=BytesIO(data),
             object_name=prefix+str(i))
         objs.append(obj)
-        swift.upload(container, objs)
+        for r in swift.upload(container, objs):
+            pass
+#            if r['success']:
+#                if 'object' in r:
+#                    print("uploaded object: %s" % (r['object']))
+#            else:
+#                error = r['error']
+#                if r['action'] == "create_container":
+#                    print(
+#                        'Warning: failed to create container '
+#                        "'%s'%s", container, error)
+#                elif r['action'] == "upload_object":
+#                    print(
+#                        "Failed to upload object %s to container %s: %s" %
+#                        (r['object'], container, error))
+#                else:
+#                    print("%s" % error)
     end = time()
 
     print("Wrote {:d} files of {:d}K in {:.2f} s".format(n, size, (end-start)))
