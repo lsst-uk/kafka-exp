76619468ba157afdba83750aba7c6383c8cb4ad6
Gareth Francis <gfrancis@MBP-GF2.local>
2020-04-21 11:06:28 +0100

    fix error handling

--- sherlock/mock_sherlock_driver.py
+++ sherlock/mock_sherlock_driver.py
@@ -1,19 +1,33 @@
+# Mock Sherlock driver
+# Based on lasair/src/sherlock_web_wrapper/server/sherlock_driver.py
+# Takes a list of name/ra/dec as input, adds ra and dec and returns the result in the 'sum' attribute
+# Returns empty output on error
+
 import sys
 import json
 from time import sleep
 
+# Time to sleep per record in ms
+delay = 20
+
 def run_sherlock(json_request):
-    reqs = json.loads(json_request)
-
     response = {}
-    for obj in reqs:
-        name = obj['name']
-        response[name] = {}
-        response[name]['ra'] = obj['ra']
-        response[name]['dec'] = obj['dec']
-        response[name]['sum'] = float(obj['ra']) + float(obj['dec'])
-        sleep(.020)
-
+    try:
+        reqs = json.loads(json_request)
+        for obj in reqs:
+            try:
+                name = obj['name']
+                ra = obj['ra']
+                dec = obj['dec']
+            except:
+                continue
+            response[name] = {}
+            response[name]['ra'] = ra
+            response[name]['dec'] = dec
+            response[name]['sum'] = float(ra) + float(dec)
+            sleep(delay/1000.0)
+    except:
+        pass
     return json.dumps(response, indent=2)
 
 
